ba3eeab3651ab29d1b80c6a36fc9d84d2026dd96
itsaz00 <116044347+itsaz00@users.noreply.github.com>
2022-10-17 20:57:01 -0400

    Initial commit

---
+++ data/temp-data.1666052735105.js
@@ -0,0 +1,1 @@
+window.tempDataJSON = {"books":true,"series":true,"collections":false,"wishlist":false,"extras":{"domain-extension":".com","pages":{},"subPageStates":[{"key":"categories","enabled":true},{"key":"authors","enabled":true},{"key":"publishers","enabled":true},{"key":"series","enabled":true},{"key":"narrators","enabled":true}],"cacheID":1666052735105}};
